8ad58b04201850a1373013d6910559fbb99962f7
vikash-parida <vikashparida067@gmail.com>
2022-12-29 10:42:17 +0530

    array

---
+++ createArray.js
@@ -0,0 +1,11 @@
+// let arr = [];   //array literals
+// const arr1 = new Array();  //array constructer
+ arr1 = [1,false,"vikash",{name:"vikash",age:25},[1,10,25,],null,undefined] // in array we can store any data type value and is not fix length
+
+let arr = ["vikash","rakesh","sunny","tajel","amrita","jarnra"]
+
+console.log(arr[0]); //array index start with 0
+
+console.log(arr[arr[arr.length-1]]); // find last index in arr  arr.length-1 
+
+
